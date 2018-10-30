ed44966a705e8a40ce91e4765aa2e9e2bd67c4f1
thejamilah <jamilahmcw@gmail.com>
2018-10-29 17:11:35 -0700

    Added requirements under Hotels; added Hotel Interface

--- Async_Inn/Async_Inn/Models/HotelRoom.cs
+++ Async_Inn/Async_Inn/Models/HotelRoom.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,15 +9,19 @@ namespace Async_Inn.Models
     public class HotelRoom
     {
         public int HotelID { get; set; }
-
+        [Required]
+        [Display(Name = "Room Number")]
         public int RoomNumber { get; set; }
-
+        [Required]
         public int RoomID { get; set; }
-
+        [Required]
+        [Display(Name = "Room Name")]
         public string RoomName { get; set; }
-
+        [Required]
+        [Display(Name = "Room Rate")]
         public decimal RoomRate { get; set; }
-
+        [Required]
+        [Display(Name = "Pet Friendly")]
         public bool PetFriendly { get; set; }
 
         //navigation properties
---
+++ Async_Inn/Async_Inn/Models/Interfaces/IHotel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Async_Inn.Models.Interfaces
+{
+    public interface IHotel
+    {
+        Task<List<Hotel>> GetHotel();
+
+        Task UpdateHotel();
+    }
+}
